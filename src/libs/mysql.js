42198170b91da1d252d6411e78c5954da7d9dde3
rei <reynaldocalani80@gmail.com>
2024-10-06 20:02:52 -0400

    configuracion variables de entorno

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,11 +2,10 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config: {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        port: 3306,
-        database: 'military_triton',
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
        
     }
 })
