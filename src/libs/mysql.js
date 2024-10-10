a4627310c8587022df55896f5a95c4fef61bd2d2
rei <reynaldocalani80@gmail.com>
2024-10-09 21:56:28 -0400

    correccion de .env.local

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,11 +2,12 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config: {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-       
+        host: process.env.NEXT_PUBLIC_DB_HOST,
+        user: process.env.NEXT_PUBLIC_DB_USER,
+        password: process.env.NEXT_PUBLIC_DB_PASSWORD,
+        database: process.env.NEXT_PUBLIC_DB_NAME,
+        timeout: 10000, // Aumentar el tiempo de espera de la conexión si es necesario
+
     }
 })
 
--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,23 @@
-// middleware.js
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
   const response = NextResponse.next();
-  response.headers.set('Access-Control-Allow-Origin', '*'); // Permite cualquier origen, o cambia esto a 'http://localhost:3000'
+
+  // Obtener el origen de la solicitud
+  const allowedOrigins = [
+    'http://localhost:3000', // Desarrollo local
+    'https://triton-blue.vercel.app' // Producción
+  ];
+
+  const origin = request.headers.get('origin');
+  
+  if (allowedOrigins.includes(origin)) {
+    response.headers.set('Access-Control-Allow-Origin', origin);
+  } else {
+    // Si el origen no está permitido, podrías devolver un error, pero en este caso lo ignoramos.
+    console.warn(`Origen no permitido: ${origin}`);
+  }
+
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   response.headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
