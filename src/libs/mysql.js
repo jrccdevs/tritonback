6a9787fd91026406210cdf644463c08614208a16
rei <reynaldocalani80@gmail.com>
2024-10-20 19:37:02 -0400

    variables de entorno

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,12 +2,12 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config: {
-        host: process.env.NEXT_PUBLIC_DB_HOST,
-        user: process.env.NEXT_PUBLIC_DB_USER,
-        password: process.env.NEXT_PUBLIC_DB_PASSWORD,
-        database: process.env.NEXT_PUBLIC_DB_NAME,
+        host: process.env.NEXT_PUBLIC_DB_HOST || "localhost",
+        user: process.env.NEXT_PUBLIC_DB_USER || "root",
+        password: process.env.NEXT_PUBLIC_DB_PASSWORD || "",
+        database: process.env.NEXT_PUBLIC_DB_NAME || "boliviandcode_tritondb",
+        port: process.env.NEXT_PUBLIC_DB_PORT || 3306,
         timeout: 10000, // Aumentar el tiempo de espera de la conexión si es necesario
 
     }
 })
-
