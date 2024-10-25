eb0f959d4cbd90d8644fd59903829c71e23f534d
rei <reynaldocalani80@gmail.com>
2024-10-24 22:08:20 -0400

    cambio de PUBLIC_NEXT

--- src/libs/mysql.js
+++ src/libs/mysql.js
@@ -2,12 +2,13 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config: {
-        host: process.env.NEXT_PUBLIC_DB_HOST || "localhost",
-        user: process.env.NEXT_PUBLIC_DB_USER || "root",
-        password: process.env.NEXT_PUBLIC_DB_PASSWORD || "",
-        database: process.env.NEXT_PUBLIC_DB_NAME || "boliviandcode_tritondb",
-        port: process.env.NEXT_PUBLIC_DB_PORT || 3306,
+        host: process.env.DB_HOST || "localhost",
+        user: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "",
+        database: process.env.DB_NAME || "boliviandcode_tritondb",
+        port: process.env.DB_PORT || 3306,
         timeout: 10000, // Aumentar el tiempo de espera de la conexión si es necesario
 
     }
 })
+
